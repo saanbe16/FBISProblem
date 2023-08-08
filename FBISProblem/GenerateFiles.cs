476a2d2ff04934f0aedf981426afa6c885c2975c
Joslin Some <joslin-some@uiowa.edu>
2023-08-08 17:11:54 -0500

    starting code to create random files for data simulation

---
+++ FBISProblem/GenerateFiles.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+namespace FBISProblem
+{
+	public class GenerateFiles
+	{
+		public GenerateFiles()
+		{
+		}
+
+        static public void Main(String[] args)
+        {
+            createInitialFileSetUp();
+            createInputFiles();
+        }
+
+        public static void createInitialFileSetUp()
+        {
+            Directory.CreateDirectory("CombinedLetters");
+            Directory.CreateDirectory("CombinedLetters/Input");
+            Directory.CreateDirectory("CombinedLetters/Input/Admission");
+            Directory.CreateDirectory("CombinedLetters/Input/Scholarship");
+            Directory.CreateDirectory("CombinedLetters/Archive");
+            Directory.CreateDirectory("CombinedLetters/Output");
+        }
+
+        public static void createInputFiles()
+        {
+            Random rnd = new Random();
+            double randNum;
+            int day = 20;
+            string studentId;
+            for (int i=0; i<5; i++) // creating test data for 5 days
+            {
+                string date = "202304"+ day.ToString();
+                day+=1;
+                for(int j=0; j < 10; j++) // assuming 10 students per day for testing
+                {
+                    studentId = "001"+rnd.Next(10000,99999).ToString();
+                    randNum = rnd.NextDouble();
+                    Console.WriteLine("StudentId ="+ studentId+ " date = "+ date+ "rand ="+ randNum);
+                    Console.ReadLine();
+                    if (randNum >0.35) // Assume 65% chance of admission for randomness
+                    {
+                        Directory.CreateDirectory("CombinedLetters/Input/Admission/"+date);
+                    }
+                    if (randNum > 0.65) // assume 35% chance of scholarship for randomness
+                    {
+                        Directory.CreateDirectory("CombinedLetters/Input/Scholarship/" + date);
+                    }
+                }
+                
+            }
+
+        }
+    }
+}
+
--- FBISProblem/Program.cs
+++ FBISProblem/Program.cs
@@ -1,11 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-int x = 2;
-if (x == 1)
-{
-    Console.WriteLine("Testing");
-}
-else {
-    Console.WriteLine("GooGooGaaGaa");
-}
-
+﻿// See https://aka.ms/new-console-template for 
