cde583cda836ef2a0c756382ee30dceeaff3c2c8
Joslin Some <joslin-some@uiowa.edu>
2023-08-08 21:04:37 -0500

    Finished file generation for testing

--- FBISProblem/GenerateFiles.cs
+++ FBISProblem/GenerateFiles.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Text;
+
 namespace FBISProblem
 {
 	public class GenerateFiles
@@ -36,20 +38,33 @@ namespace FBISProblem
                 day+=1;
                 for(int j=0; j < 10; j++) // assuming 10 students per day for testing
                 {
-                    studentId = "001"+rnd.Next(10000,99999).ToString();
+                    studentId = "01"+rnd.Next(100000,999999).ToString();
                     randNum = rnd.NextDouble();
                     Console.WriteLine("StudentId ="+ studentId+ " date = "+ date+ "rand ="+ randNum);
-                    Console.ReadLine();
                     if (randNum >0.35) // Assume 65% chance of admission for randomness
                     {
                         Directory.CreateDirectory("CombinedLetters/Input/Admission/"+date);
+                        // Creating admission file
+                        using (FileStream fs = File.Create("CombinedLetters/Input/Admission/" + date+"/"+"admission-"+studentId+".txt"))
+                        {
+                            byte[] info = new UTF8Encoding(true).GetBytes("CONGRATULATIONS! \n--------------------------\nCongrats student "+ studentId+", you have been admitted to the university of Iowa\n\nGo Hawks!");
+                            // Add some information to the file.
+                            fs.Write(info, 0, info.Length);
+                        }
                     }
                     if (randNum > 0.65) // assume 35% chance of scholarship for randomness
                     {
                         Directory.CreateDirectory("CombinedLetters/Input/Scholarship/" + date);
+                        // Creating scholarship file
+                        using (FileStream fs = File.Create("CombinedLetters/Input/Scholarship/" + date + "/" + "scholarship-" + studentId + ".txt"))
+                        {
+                            byte[] info = new UTF8Encoding(true).GetBytes("CONGRATULATIONS! \n--------------------------\nCongrats student "+ studentId+", you have been awarded a scholarship to the University of Iowa\n\nWe hope top see you soon!");
+                            // Add some information to the file.
+                            fs.Write(info, 0, info.Length);
+                        }
                     }
                 }
-                
+
             }
 
         }
