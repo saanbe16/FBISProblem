53034276b31ec1a5563f79afba017eea560dcb67
Joslin Some <joslin-some@uiowa.edu>
2023-08-08 22:36:45 -0500

    found where/when to combine files

--- FBISProblem/Solution.cs
+++ FBISProblem/Solution.cs
@@ -6,12 +6,48 @@ namespace FBISProblem
         static public void Main(String[] args)
         {
             FileGenerator generator = new FileGenerator();
-            generator.generate();
+            //generator.generate();
             Console.WriteLine("Generated new files");
-             
+
+            // Check for past days
+            HashSet<string> admissionDirSet = new HashSet<string>();
+            var directories = Directory.GetDirectories("CombinedLetters/Input/Admission");
+            foreach(string dir in directories)
+            {
+                admissionDirSet.Add(dir.Substring(dir.Length - 8)); // add date folder name
+
+            }
+            directories = Directory.GetDirectories("CombinedLetters/Input/Scholarship");
+            foreach (string dir in directories)
+
+            {
+                string date = dir.Substring(dir.Length - 8);
+                if (admissionDirSet.Contains(date))
+                {
+                    string admissionDirectory = dir.Replace("Scholarship","Admission");
+                    Console.WriteLine(admissionDirectory);
+                    HashSet<string> admissionFiles = new HashSet<string>();
+                    var directory = new DirectoryInfo(admissionDirectory);
+                    foreach (FileInfo fi in directory.GetFiles()) // adding to map
+                    {
+                        admissionFiles.Add(fi.Name.Substring(fi.Name.Length-12));
+                    }
+                    directory = new DirectoryInfo(dir);
+                    foreach (FileInfo fi in directory.GetFiles()) // adding to map
+                    {
+                        string file = fi.Name.Substring(fi.Name.Length - 12);
+                        if (admissionFiles.Contains(file))
+                        {
+                            // COMBINATION CODE HERE
+                        }
+                    }
+                }
+            }
+
+            Console.Read();
 
         }
 
-	}
+    }
 }
 
